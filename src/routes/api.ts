b42a85d220edaddc0b1c11c9b1c790ce26f71c08
Corey Innis <corey@geometer.io>
2021-04-23 17:02:17 -0700

    Hack: browser launch works

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,29 @@
-import * as express from "express";
+import * as express    from "express";
+import * as playwright from "playwright";
 
 export const register = (app: express.Application) => {
+  app.get("/api/browse", async (req: any, res) => {
+    try {
+      (async () => {
+        const browser = await playwright.chromium.launch();
+        const page    = await browser.newPage();
+        const domain  = "google.com";
+
+        await page.goto(`https://${domain}`);
+        await page.screenshot({
+          path: `doc/${domain}.png`
+        });
+
+        await browser.close();
+      })();
+
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ x: "OK" });
+    } catch (e) {
+      // handle errors
+    }
+  });
+
   app.get("/api/status", async (req: any, res) => {
     try {
       return res.json({
