d416a3ea65567bb38a8fa5669f0875af76522220
Corey Innis <corey@geometer.io>
2021-04-24 12:22:34 -0700

    Close playwright servers

--- src/routes/ws.ts
+++ src/routes/ws.ts
@@ -9,9 +9,6 @@ export const register = (app: express.Application) => {
   // BrowserServer. This might actually be desired, in the interest of test
   // isolation. Or, perhaps we want to maintain a pool for the sake of speed
   // and resource constraints.
-  //
-  // In any event, the next step is to ensure that these servers are closed
-  // after the test run.
   app.get("/chromium", async (req: any, res) => {
     const server = await playwright.chromium.launchServer();
     console.info(`proxying '/chromium' to '${server.wsEndpoint()}'`)
@@ -22,8 +19,28 @@ export const register = (app: express.Application) => {
       ignorePath: true
     });
 
+    // req.socket.on("close", () => {
+    //   console.info("socket event: close");
+    // });
+
+    // req.socket.on("data", () => {
+    //   console.info("socket event: data");
+    // });
+
+    // req.socket.on("drain", () => {
+    //   console.info("socket event: drain");
+    // });
+
     proxy.ws(req, req.socket, req.headers);
 
-    // server.close();
+    // socket event: data
+    // proxy event: close
+    // socket event: close
+    // ...so, close the server on proxy close, or socket close?
+    proxy.on("close", () => {
+      console.info("proxy event: close");
+      console.info("closing playwright server");
+      server.close()
+    });
   });
 };
